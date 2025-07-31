c38d827337341a52bae77b7188df8fcb165374ca
Alexandria Harp <76006661+alexharp3@users.noreply.github.com>
2025-07-31 15:45:56 -0500

    Add files via upload

---
+++ src/libraryManagementSystem/Request.java
@@ -0,0 +1,24 @@
+package libraryManagementSystem;
+
+import java.time.LocalDate;
+
+public class Request {
+    private User requester;
+    private Materials material;
+    private LocalDate date;
+
+    public Request(User requester, Materials material) {
+        this.requester = requester;
+        this.material = material;
+        this.date = LocalDate.now();
+    }
+
+    public User getRequester() {
+        return requester;
+    }
+
+    public Materials getMaterial() {
+        return material;
+    }
+}
+
